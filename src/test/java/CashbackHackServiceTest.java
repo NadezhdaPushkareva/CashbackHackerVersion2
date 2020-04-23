dd6f1ba6eb0dd9c89c2d6d449dc041be238cfab3
Nadezda Pushkareva <NadezdaPushkareva@yandex.ru>
2020-04-23 23:48:25 +0400

    Initial commit

---
+++ src/test/java/CashbackHackServiceTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+    @Test
+    void remainIfAmountIs900() {
+
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 900;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 100;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void remainIfAmountIs1000() {
+
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void remainIfAmountIs0() {
+
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 0;
+        assertThrows(IllegalArgumentException.class, () -> cashbackHackService.remain(amount));
+    }
+}
